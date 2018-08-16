bc3c8e2b864cbfd7c0eeb59ceda25a4031382da7
Mayu14 <b4123504@edu.kit.ac.jp>
2018-08-16 19:11:16 +0900

    形状データ数の最適値を探索できるように変更

--- training.py
+++ training.py
@@ -59,101 +59,110 @@ def get_case_number(source, env, case_number):
     return str(case_number).zfill(5)
 
 def main(fname_lift_train, fname_shape_train, fname_lift_test, fname_shape_test, case_number, case_type=3, env="Lab"):
-    old_session = KTF.get_session()
+    r_rate = [1, 2, 4, 8]
+    for rr in r_rate:
+        if rr == 1:
+            s_odd = 0   # 全部読みだす
+        elif fname_shape_train.find("fourier") != -1:
+            s_odd = 3   # 前方から読み出す(fourier用)
+        else:
+            s_odd = 4   # 全体にわたって等間隔に読み出す(equidistant, dense用)
 
-    with tf.Graph().as_default():
-        source = "Incompressible_Invicid\\training_data\\"
-        if env == "Lab":
-            source = "D:\\Toyota\\Data\\" + source
-            case_num = get_case_number(source, env, case_number)
-            log_name = "learned\\" + case_num + "_tb_log.hdf5"
-            json_name = "learned\\" + case_num + "_mlp_model_.json"
-            weight_name = "learned\\" + case_num + "_mlp_weight.h5"
-        elif env == "Colab":
-            source = "/content/drive/Colab Notebooks/" + source.replace("\\", "/")
-            case_num = get_case_number(source, env, case_number)
-            log_name = "learned/" + case_num + "_log.hdf5"
-            json_name = "learned/" + case_num + "_mlp_model_.json"
-            weight_name = "learned/" + case_num + "_mlp_weight.h5"
+        old_session = KTF.get_session()
 
-        session = tf.Session('')
-        KTF.set_session(session)
-        KTF.set_learning_phase(1)
+        with tf.Graph().as_default():
+            source = "Incompressible_Invicid\\training_data\\"
+            if env == "Lab":
+                source = "D:\\Toyota\\Data\\" + source
+                case_num = get_case_number(source, env, case_number)
+                log_name = "learned\\" + case_num + "_tb_log.hdf5"
+                json_name = "learned\\" + case_num + "_mlp_model_.json"
+                weight_name = "learned\\" + case_num + "_mlp_weight.h5"
+            elif env == "Colab":
+                source = "/content/drive/Colab Notebooks/" + source.replace("\\", "/")
+                case_num = get_case_number(source, env, case_number)
+                log_name = "learned/" + case_num + "_log.hdf5"
+                json_name = "learned/" + case_num + "_mlp_model_.json"
+                weight_name = "learned/" + case_num + "_mlp_weight.h5"
 
-        model = Sequential()
-        if case_type == 3:
-            X_train, y_train = read_csv_type3(source, fname_lift_train, fname_shape_train, shape_odd = 0, read_rate = 1)
-            x_test, y_test = read_csv_type3(source, fname_lift_test, fname_shape_test, shape_odd=0, read_rate=1)
+            session = tf.Session('')
+            KTF.set_session(session)
+            KTF.set_learning_phase(1)
 
-        input_vector_dim = X_train.shape[1]
-        with tf.name_scope("inference") as scope:
-            model.add(Dense(units=2, input_dim=input_vector_dim))
-            model.add(LeakyReLU())
-            model.add(Dense(units=16))
-            model.add(LeakyReLU())
-            """
-            model.add(Dense(units=128))
-            model.add(LeakyReLU())
-            model.add(Dense(units=192))
-            model.add(LeakyReLU())
-            model.add(Dense(units=2048))
-            model.add(LeakyReLU())
-            model.add(Dense(units=2048))
-            model.add(LeakyReLU())
-            """
-            """
-            model.add(Dense(units=512))
-            model.add(LeakyReLU())
+            model = Sequential()
+            if case_type == 3:
+                X_train, y_train = read_csv_type3(source, fname_lift_train, fname_shape_train, shape_odd = s_odd, read_rate = rr)
+                x_test, y_test = read_csv_type3(source, fname_lift_test, fname_shape_test, shape_odd=s_odd, read_rate=rr)
 
-            for i in range(5):
-                model.add(Dense(units = 512))
+            input_vector_dim = X_train.shape[1]
+            with tf.name_scope("inference") as scope:
+                model.add(Dense(units=2, input_dim=input_vector_dim))
                 model.add(LeakyReLU())
+                model.add(Dense(units=16))
+                model.add(LeakyReLU())
+                """
+                model.add(Dense(units=128))
+                model.add(LeakyReLU())
+                model.add(Dense(units=192))
+                model.add(LeakyReLU())
+                model.add(Dense(units=2048))
+                model.add(LeakyReLU())
+                model.add(Dense(units=2048))
+                model.add(LeakyReLU())
+                """
+                """
+                model.add(Dense(units=512))
+                model.add(LeakyReLU())
+    
+                for i in range(5):
+                    model.add(Dense(units = 512))
+                    model.add(LeakyReLU())
+                    # model.add(Dropout(0.5))
+                # model.add(Dense(units=half, activation='relu'))
                 # model.add(Dropout(0.5))
-            # model.add(Dense(units=half, activation='relu'))
-            # model.add(Dropout(0.5))
+                """
+                model.add(Dense(units=1))
+
+            model.summary()
+
+            save_my_log(source, case_number, fname_lift_train, fname_shape_train, model.summary())
+            # es_cb = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+            tb_cb = TensorBoard(log_dir=source + log_name, histogram_freq=0, write_grads=True)
+
+            model.compile(loss="mean_squared_error",
+                          optimizer='Adam')
+
+            batch_size = 500
+            train_steps, train_batches = batch_iter(X_train, y_train, batch_size)
+            valid_steps, valid_batches = batch_iter(x_test, y_test, batch_size)
             """
-            model.add(Dense(units=1))
+            model.fit(x=X_train, y=y_train,
+                      batch_size=600, nb_epoch=1000,
+                      validation_split=0.05, callbacks=[tb_cb])
+            """
+            model.fit_generator(train_batches, train_steps,
+                                epochs=1000,
+                                validation_data=valid_batches,
+                                validation_steps=valid_steps,
+                                callbacks=[tb_cb])
+            # X_train: [number, angle, shape001, shape002, ..., shapeMAX]
+            # y_train: [number, lift]
+            # 適当に中央付近の翼を抜き出しての-40-38degreeをプロットさせてみる
+            tekito = 1306 * 40  # NACA2613 or NACA2615
+            plt.plot(X_train[tekito:tekito+40, 0], y_train[tekito:tekito+40])
+            plt.plot(X_train[tekito:tekito+40, 0], model.predict(X_train)[tekito:tekito+40])
+            plt.savefig(source + case_num + "_train.png")
 
-        model.summary()
+            y_predict = model.predict(x_test)
+            tekito = (99 + 13) * 40 # 22012
+            plt.plot(x_test[tekito:tekito+40, 0], y_test[tekito:tekito+40])
+            plt.plot(x_test[tekito:tekito+40, 0], y_predict[tekito:tekito+40])
+            plt.savefig(source + case_num + "_test.png")
 
-        save_my_log(source, case_number, fname_lift_train, fname_shape_train, model.summary())
-        # es_cb = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
-        tb_cb = TensorBoard(log_dir=source + log_name, histogram_freq=0, write_grads=True)
-
-        model.compile(loss="mean_squared_error",
-                      optimizer='Adam')
-
-        batch_size = 500
-        train_steps, train_batches = batch_iter(X_train, y_train, batch_size)
-        valid_steps, valid_batches = batch_iter(x_test, y_test, batch_size)
-        """
-        model.fit(x=X_train, y=y_train,
-                  batch_size=600, nb_epoch=1000,
-                  validation_split=0.05, callbacks=[tb_cb])
-        """
-        model.fit_generator(train_batches, train_steps,
-                            epochs=1000,
-                            validation_data=valid_batches,
-                            validation_steps=valid_steps,
-                            callbacks=[tb_cb])
-        # X_train: [number, angle, shape001, shape002, ..., shapeMAX]
-        # y_train: [number, lift]
-        # 適当に中央付近の翼を抜き出しての-40-38degreeをプロットさせてみる
-        tekito = 1306 * 40  # NACA2613 or NACA2615
-        plt.plot(X_train[tekito:tekito+40, 0], y_train[tekito:tekito+40])
-        plt.plot(X_train[tekito:tekito+40, 0], model.predict(X_train)[tekito:tekito+40])
-        plt.savefig(source + case_num + "_train.png")
-
-        y_predict = model.predict(x_test)
-        tekito = (99 + 13) * 40 # 22012
-        plt.plot(x_test[tekito:tekito+40, 0], y_test[tekito:tekito+40])
-        plt.plot(x_test[tekito:tekito+40, 0], y_predict[tekito:tekito+40])
-        plt.savefig(source + case_num + "_test.png")
-
-    json_string = model.to_json()
-    open(source + json_name, 'w').write(json_string)
-    model.save_weights(source + weight_name)
-    KTF.set_session(old_session)
+        json_string = model.to_json()
+        open(source + json_name, 'w').write(json_string)
+        model.save_weights(source + weight_name)
+        KTF.set_session(old_session)
 
 
 if __name__ == '__main__':
