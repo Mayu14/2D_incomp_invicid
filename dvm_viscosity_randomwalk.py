90bbdffc1562a9db20fcc0a66abf332b8ab61115
Mayu14 <b4123504@edu.kit.ac.jp>
2018-09-20 01:12:26 +0900

    粘性計算用プログラム(予定地)

---
+++ dvm_viscosity_randomwalk.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+import numpy as np
+import scipy.linalg.decomp_qr as qr
+from naca_4digit_test import Naca_4_digit, Naca_5_digit
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
