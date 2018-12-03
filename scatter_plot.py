927f175b7e391e697d20142c0c286dba85ea28ab
Mayu14 <b4123504@edu.kit.ac.jp>
2018-12-03 17:03:16 +0900

    Fix:デフォルトで出力結果を対数スケールにするオプションlog_scaleを追加

--- scatter_plot.py
+++ scatter_plot.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def make_scatter_plot(data_a, data_b, label_a, label_b, resolution=100, path="", fname="scat"):
+def make_scatter_plot(data_a, data_b, label_a, label_b, resolution=200, path="", fname="scat", log_scale=True):
     size = data_a.shape[0]
     size_b = data_b.shape[0]
     if size != size_b:
@@ -29,6 +29,10 @@ def make_scatter_plot(data_a, data_b, label_a, label_b, resolution=100, path="",
 
     for i in range(size):
         scatter[int(data_scat_a[i]), int(data_scat_b[i])] += 1
+    
+    if log_scale:
+        scatter += 1
+        scatter = np.log(scatter)
 
     imshow = False
     plt.figure()
@@ -38,7 +42,7 @@ def make_scatter_plot(data_a, data_b, label_a, label_b, resolution=100, path="",
     else:
         plt.pcolormesh(label, label, scatter, cmap='jet')
         pp = plt.colorbar(orientation="vertical")
-        pp.set_label("Number of times", fontname="Arial", fontsize=24)
+        pp.set_label("Number of times (log)", fontname = "Arial", fontsize = 12)
     plt.xlabel(label_a)
     plt.ylabel(label_b)
 
@@ -50,7 +54,7 @@ def make_scatter_plot(data_a, data_b, label_a, label_b, resolution=100, path="",
     else:
         plt.pcolormesh(label, label, scatter, cmap='jet')
         pp = plt.colorbar(orientation = "vertical")
-        pp.set_label("Number of times", fontname = "Arial", fontsize = 24)
+        pp.set_label("Number of times (log)", fontname = "Arial", fontsize = 12)
     plt.xlabel(label_a)
     plt.ylabel(label_b)
 
@@ -75,4 +79,4 @@ if __name__ == '__main__':
     a, b = test()
     label_a = "a"
     label_b = "b"
-    make_scatter_plot(a, b, label_a, label_b, resolution=100)
+    make_scatter_plot(a, b, label_a, label_b, resolution=200)
